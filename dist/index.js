6fb9e630309dea95b25507c759cbddeb8f46b18e
Steve Thompson <writetome51@gmail.com>
2020-08-04 00:42:54 -0600

    minor change

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 import { getSum } from '@writetome51/get-sum-average-product';
 import { getArrayFromProperty } from '@writetome51/get-array-from-property';
+
+
 // Returns sum of all `propertyWithNumberValue` found in objects.
 // `propertyWithNumberValue` can contain dot-notation.
+
 export function getSumFromProperty(propertyWithNumberValue, objects) {
     let numbers = getArrayFromProperty(propertyWithNumberValue, objects);
     return getSum(numbers);
